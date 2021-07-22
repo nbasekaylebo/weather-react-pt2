a3c6b84e57860102f3b8e1d17c2f7cdc5ea256a6
nbasekaylebo <nannalebo@gmail.com>
2021-07-22 15:03:14 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Nastassja Basekay Wa Basekay and is
-        open-sourced on{" "}
-        <a
-          href="https://github.com/nbasekaylebo/weather-react-pt2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Nastassja Basekay Wa Basekay and is
+          open-sourced on{" "}
+          <a
+            href="https://github.com/nbasekaylebo/weather-react-pt2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
