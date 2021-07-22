0093204c0f0365910bd18fa663228d3cafe5e77f
nbasekaylebo <nannalebo@gmail.com>
2021-07-22 14:35:31 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Nastassja Basekay Wa Basekay and is
+        open-sourced on{" "}
+        <a
+          href="https://github.com/nbasekaylebo/weather-react-pt2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
