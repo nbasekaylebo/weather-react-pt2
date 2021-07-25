24e392ddc06c5b90f2fd351dd331b3499f1ce907
nbasekaylebo <nannalebo@gmail.com>
2021-07-25 21:26:25 +0100

    Added API intergration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="London" />
         <footer>
           This project was coded by Nastassja Basekay Wa Basekay and is
           open-sourced on{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,91 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="Weather-wrapper">
-        <div className="background">
-          <div className="Weather-app">
-            <form id="search-form" className="mb-3">
-              <div className="row">
-                <div className="col-6 mb-3 mb-sm-0">
-                  <input
-                    type="search"
-                    placeholder="Check your city"
-                    className="form-control"
-                    id="city-input"
-                    autoComplete="off"
-                    autoFocus="on"
-                  />
-                </div>
-                <div className="col-3 col-sm-2">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-outline-secondary w-100"
-                  />
-                </div>
-                <div className="col-3 col-sm-2">
-                  <button
-                    className="btn btn-outline-secondary w-100"
-                    id="current-location-button"
-                  >
-                    Current
-                  </button>
-                </div>
-              </div>
-            </form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-            <div className="row flex-column-reverse flex-sm-row">
-              <div className="col-12 col-sm-6 mini-card">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="overview">
-                      <h1 id="city">London</h1>
-                      <ul>
-                        <li>
-                          <span id="date">Thursday 17:00</span>
-                        </li>
-                        <li id="description">Sunny</li>
-                      </ul>
-                    </div>
-                    <div className="row">
-                      <div className="col-6">
-                        <div className="clearfix weather-temperature">
-                          <div className="float-left">
-                            <span className="temperature">25</span>
-                            <span className="units"> °C</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-6">
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      date: "Thursday 17:00",
+      precipitation: 1,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="Weather-wrapper">
+          <div className="background">
+            <div className="Weather-app">
+              <form id="search-form" className="mb-3">
+                <div className="row">
+                  <div className="col-6 mb-3 mb-sm-0">
+                    <input
+                      type="search"
+                      placeholder="Check your city"
+                      className="form-control"
+                      id="city-input"
+                      autoComplete="off"
+                      autoFocus="on"
+                    />
+                  </div>
+                  <div className="col-3 col-sm-2">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-outline-secondary w-100"
+                    />
+                  </div>
+                  <div className="col-3 col-sm-2">
+                    <button
+                      className="btn btn-outline-secondary w-100"
+                      id="current-location-button"
+                    >
+                      Current
+                    </button>
+                  </div>
+                </div>
+              </form>
+
+              <div className="row flex-column-reverse flex-sm-row">
+                <div className="col-12 col-sm-6 mini-card">
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="overview">
+                        <h1 id="city">{weatherData.city}</h1>
                         <ul>
                           <li>
-                            Precipitation: <span id="precipitation">0</span>%
+                            <span id="date">{weatherData.date}</span>
                           </li>
-                          <li>
-                            Humidity: <span id="humidity">0</span>%
-                          </li>
-                          <li>
-                            Wind: <span id="wind">3</span> km/h
-                          </li>
+                          <li id="description">{weatherData.description}</li>
                         </ul>
+                      </div>
+                      <div className="row">
+                        <div className="col-6">
+                          <div className="clearfix weather-temperature">
+                            <div className="float-left">
+                              <span className="temperature">
+                                {Math.round(weatherData.temperature)}
+                              </span>
+                              <span className="units"> °C</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-6">
+                          <ul>
+                            <li>Precipitation: {weatherData.precipitation}%</li>
+                            <li>Humidity: {weatherData.humidity}%</li>
+                            <li>Wind: {weatherData.wind} km/h</li>
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+              <img
+                className="todays-icon col-12 col-sm-4"
+                src={weatherData.iconUrl}
+                id="icon"
+                alt={weatherData.descripttion}
+              />
             </div>
-            <img
-              className="todays-icon col-12 col-sm-4"
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              id="icon"
-              alt="weather icon"
-            />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "164d437e2d00bd82847b8e871b123f28";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
