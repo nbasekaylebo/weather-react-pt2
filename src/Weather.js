5e0b5a4c054340006116144e4aa9d8e0c71eff4f
nbasekaylebo <nannalebo@gmail.com>
2021-07-25 22:52:38 +0100

    moved some parts in to a new component

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -15,9 +15,24 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       city: response.data.name,
     });
+  }
+
+  function search() {
+    const apiKey = "164d437e2d00bd82847b8e871b123f28";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.ready) {
@@ -26,7 +41,7 @@ export default function Weather(props) {
         <div className="Weather-wrapper">
           <div className="background">
             <div className="Weather-app">
-              <form id="search-form" className="mb-3">
+              <form onSubmit={handleSubmit} className="mb-3">
                 <div className="row">
                   <div className="col-6 mb-3 mb-sm-0">
                     <input
@@ -36,6 +51,7 @@ export default function Weather(props) {
                       id="city-input"
                       autoComplete="off"
                       autoFocus="on"
+                      onChange={handleCityChange}
                     />
                   </div>
                   <div className="col-3 col-sm-2">
@@ -55,59 +71,14 @@ export default function Weather(props) {
                   </div>
                 </div>
               </form>
-
-              <div className="row flex-column-reverse flex-sm-row">
-                <div className="col-12 col-sm-6 mini-card">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="overview">
-                        <h1 id="city">{weatherData.city}</h1>
-                        <ul>
-                          <li>
-                            <FormattedDate date={weatherData.date} />
-                          </li>
-                          {weatherData.description}
-                        </ul>
-                      </div>
-                      <div className="row">
-                        <div className="col-6">
-                          <div className="clearfix weather-temperature">
-                            <div className="float-left">
-                              <span className="temperature">
-                                {Math.round(weatherData.temperature)}
-                              </span>
-                              <span className="units"> °C</span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="col-6">
-                          <ul>
-                            <li>Precipitation: {weatherData.precipitation}%</li>
-                            <li>Humidity: {weatherData.humidity}%</li>
-                            <li>Wind: {weatherData.wind} km/h</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <img
-                className="todays-icon col-12 col-sm-4"
-                src={weatherData.iconUrl}
-                id="icon"
-                alt={weatherData.descripttion}
-              />
+              <WeatherInfo data={weatherData} />
             </div>
           </div>
         </div>
       </div>
     );
   } else {
-    const apiKey = "164d437e2d00bd82847b8e871b123f28";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,51 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row flex-column-reverse flex-sm-row">
+        <div className="col-12 col-sm-6 mini-card">
+          <div className="card">
+            <div className="card-body">
+              <div className="overview">
+                <h1 id="city">{props.data.city}</h1>
+                <ul>
+                  <li>
+                    <FormattedDate date={props.data.date} />
+                  </li>
+                  {props.data.description}
+                </ul>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <div className="clearfix weather-temperature">
+                    <div className="float-left">
+                      <span className="temperature">
+                        {Math.round(props.data.temperature)}
+                      </span>
+                      <span className="units"> °C</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <ul>
+                    <li>Precipitation: {props.data.precipitation}%</li>
+                    <li>Humidity: {props.data.humidity}%</li>
+                    <li>Wind: {props.data.wind} km/h</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img
+        className="todays-icon col-12 col-sm-4"
+        src={props.data.iconUrl}
+        id="icon"
+        alt={props.data.descripttion}
+      />
+    </div>
+  );
+}
